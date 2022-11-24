419f10f1068fce05c969e7b13e9c4fe0659a5fdb
noman <nomannabil30@gmail.com>
2022-11-24 10:35:53 +0600

    mongodb database added successfully

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cors = require('cors');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config();
 
 const port = process.env.PORT || 5000;
@@ -9,6 +10,28 @@ const app = express();
 // Middleware
 app.use(cors());
 app.use(express.json());
+
+// ----------------------------
+// MongoDB connection
+
+const uri = process.env.DB_URL;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+async function runDB() {
+	try {
+		const connection = await client.connect();
+		if (connection) {
+			console.log('Database connected successfully');
+		}
+	} catch (error) {
+		console.error(error.stack);
+	}
+}
+runDB();
+const database = client.db('CarBazar');
+const collectionBrands = database.collection('brands');
+const collectionCars = database.collection('cars');
+// ----------------------------
 
 
 // Test Route
