d6d008ba024ec66cf5e7e13141d5599c58c73d3e
noman <nomannabil30@gmail.com>
2022-11-28 14:43:39 +0600

    JWT setup added for authorization

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const cors = require('cors');
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config();
+const jwt = require('jsonwebtoken');
 const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
 
 const port = process.env.PORT || 5000;
@@ -11,6 +12,61 @@ const app = express();
 // Middleware
 app.use(cors());
 app.use(express.json());
+
+// Middleware(Authentication)-JWT(Token)
+// --------------------------------------
+app.post('/jwt', async (req, res) => {
+	try {
+		const authoriseUser = req.body;
+		// console.log('authoriseUser', authoriseUser);
+		const query = { email: authoriseUser.email };
+		const user = usersCollection.findOne(query);
+		if (user) {
+			const token = jwt.sign(authoriseUser, process.env.ACCESS_TOKEN_SECRET_KEY, {
+				expiresIn: '24h'
+			});
+			// Send token as an object form
+			res.status(200).send({ accessToken: token });
+		} else {
+			res.status(404).send('User not found');
+		}
+	} catch (error) {
+		res.send({
+			errorDetails: error.stack
+		});
+	}
+});
+
+// 2. verifyToken(Wraper function)
+function verifyToken(req, res, next) {
+	try {
+		const authBearerToken = req.headers.authorization;
+		// console.log(authBearerToken);
+
+		if (!authBearerToken) {
+			return res.status(401).send({
+				error: 'Unauthorise Access',
+				message: 'authBearerToken not found at verifyToken function'
+			});
+		}
+		// verifyToken
+		const token = authBearerToken.split(' ')[1];
+		// console.log(token);
+		jwt.verify(token, process.env.ACCESS_TOKEN_SECRET_KEY, function (err, decoded) {
+			if (err) {
+				return res.status(403).send({
+					error: 'Forbidden Access',
+					message: 'Error found at matching token & ACCESS_TOKEN_SECRET_KEY'
+				});
+			}
+			req.decodedData = decoded;
+			// console.log('Decoded', req.decoded);
+			next();
+		});
+	} catch (error) {
+		res.send(error.stack);
+	}
+}
 
 // Payment Gateway - Stripe API
 // ----------------------------
@@ -298,28 +354,44 @@ app.delete('/reported-items/:id', async (req, res) => {
 app.post('/payments', async (req, res) => {
 	try {
 		const paymentData = req.body;
-		// console.log(paymentData);
-		const result = await paymentsCollection.insertOne(paymentData);
+		const carId = paymentData.orderData.carInfo._id;
+		// console.log(carId);
+		const filterCarId = { 'orderData.carInfo._id': carId };
+		// console.log(filterCarId);
+		const findItem = await paymentsCollection.findOne(filterCarId);
+		if (findItem) {
+			// console.log('Item found');
+			return res.send({ findItem, message: 'Payment Not Complete...!Product already Sold...!' });
+		} else {
+			const result = await paymentsCollection.insertOne(paymentData);
 
-		const orderId = paymentData.orderData._id;
-		const query = { _id: ObjectId(orderId) };
+			const orderId = paymentData.orderData._id;
+			const query = { _id: ObjectId(orderId) };
 
-		const updateDoc = {
-			$set: {
-				paymentStatus: 'paid'
-			}
-		};
-		const updateResult = await bookingsCollection.updateOne(query, updateDoc);
+			const updateDoc = {
+				$set: {
+					paymentStatus: 'paid'
+				}
+			};
+			const updateBooking = await bookingsCollection.updateOne(query, updateDoc);
 
-		const productId = paymentData.orderData.carInfo._id;
-		const filter = { _id: ObjectId(productId) };
-		const updateProduct = {
-			$set: {
-				sales_status: 'sold'
-			}
-		};
-		const updateProductResult = await carsCollection.updateOne(filter, updateProduct);
-		res.send({ result, updateResult, updateProductResult });
+			const productId = paymentData.orderData.carInfo._id;
+			const filter = { _id: ObjectId(productId) };
+
+			const updateProduct = {
+				$set: {
+					sales_status: 'sold'
+				}
+			};
+			const updateCars = await carsCollection.updateOne(filter, updateProduct);
+
+			const advertiseId = paymentData.orderData.carInfo._id;
+			const queryId = { 'product._id': advertiseId };
+
+			const delAdd = await advertisesCollection.deleteOne(queryId);
+
+			res.send({ result, updateBooking, updateCars, delAdd, message: 'Payment Complete Successfull...!' });
+		}
 	} catch (error) {
 		console.error(error.stack);
 	}
@@ -328,7 +400,25 @@ app.post('/payments', async (req, res) => {
 app.post('/advertise', async (req, res) => {
 	try {
 		const advertiseData = req.body;
-		const result = await advertisesCollection.insertOne(advertiseData);
+		const id = advertiseData.product._id;
+		console.log(id);
+		const query = { 'product._id': id };
+		const findItem = await advertisesCollection.findOne(query);
+		if (findItem) {
+			return res.send({ message: 'Already Advertised' });
+		} else {
+			const result = await advertisesCollection.insertOne(advertiseData);
+			res.send(result);
+		}
+	} catch (error) {
+		console.log(error);
+	}
+});
+
+app.get('/advertise', async (req, res) => {
+	try {
+		const query = {};
+		const result = await advertisesCollection.find(query).toArray();
 		res.send(result);
 	} catch (error) {
 		console.log(error);
